9c36660f9e2845b12895a8284e09dd5debb8f36f
qaissmasarwi <qaissm@campus.technion.ac.il>
2025-05-05 11:41:34 +0300

    adding the transaction dump info and transaction hashed message to the Transaction.cpp file

---
+++ Transaction.cpp
@@ -0,0 +1,20 @@
+
+#pragma once
+
+#include <string>
+#include <fstream>
+#include <Utilities.h>
+#include "Transaction.h"
+using std::endl;
+using std::ofstream;
+
+void TransactionDumpInfo(const Transaction& transaction, ofstream& file){
+    if (file.is_open())
+    file << "Sender Name: " << transaction.sender << endl;
+    file << "Receiver Name: " << transaction.receiver << endl;
+    file << "Transaction Value: " << transaction.value << endl;
+}
+
+string TransactionHashedMessage(const Transaction& transaction){
+    return hash(transaction.value,transaction.sender,transaction.receiver);
+}
